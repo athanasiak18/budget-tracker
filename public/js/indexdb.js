05b7f3248fb2972de8be98081c516edf50bf3b06
athanasiak18 <95444521+athanasiak18@users.noreply.github.com>
2022-06-02 15:26:34 -0400

    Update indexdb.js

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -1,5 +1,49 @@
-//offline code
-
 let db;
 
-const request = indexedDB.open('budget', 1);
+const request = indexDB.open('budget', 1);
+
+request.onupgradeneeded = function(e) {
+    const db = e.target.result;
+    db.createObjectStore("budget_data", { autoIncrement: true });
+  };
+
+  request.onsuccess = function(e) {
+    db = e.target.result;
+    if (navigator.onLine) {refresh();} 
+  };
+  request.onerror = function(e) {
+    console.log("N/A" + e.target.errorCode);
+  };
+
+  function saveRecord(record) {
+    const transaction = db.transaction(["budget_data"], "readwrite"); 
+    const store = transaction.objectStore("budget_data"); 
+    store.add(record);
+  };
+
+function refresh() {
+    const transaction = db.transaction(["budget_data"], "readwrite");
+    const store = transaction.objectStore("budget_data");
+    const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["budget_data"], "readwrite");
+        const store = transaction.objectStore("budget_data"); 
+        store.clear(); 
+      });
+    }
+  };
+};
+
+window.addEventListener("online", refresh);
